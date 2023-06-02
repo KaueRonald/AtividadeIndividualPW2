9b6e503791683132afe7aa4dada96cb26f04d020
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-06-01 22:41:04 -0300

    feat: Componente de avatar

---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,8 @@
+export const Avatar = () => {
+    return (
+        <div>
+            <img className='avatar' src="https://github.com/kaueronald.png" />
+            <img className='star' src="src/assets/rating.svg"></img> 
+        </div>
+    )
+}
